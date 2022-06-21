e0c94b700d6f248482637fcc206eacb08c061db8
Laura Oppermann <laura.oppermann98@gmx.de>
2022-06-21 02:15:04 +0200

    change port back because of failure

--- springboot/src/main/java/com/eventlistener/api/controller/BladeRegistryController.java
+++ springboot/src/main/java/com/eventlistener/api/controller/BladeRegistryController.java
@@ -8,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
